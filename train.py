754063b0d65c220e4d72e97bc7db94fbbc66e732
Harold <pusaaa80@gmail.com>
2023-06-04 23:01:56 +0800

    deleted

--- train.py
+++
@@ -1,7 +0,0 @@
-from ultralytics import YOLO
-
-def main():
-    model = YOLO('chili.pt')
-    model.train(data="qq.v2i.yolov8\data.yaml", epochs=100, imgsz=640)
-if __name__ == '__main__':
-    main()
